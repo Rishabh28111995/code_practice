212d25d4c2b6359af00b1aa6122dfcd5db832ac6
rishabhcenation <rishabhcenation28@gmail.com>
2022-11-24 18:22:00 +0530

    commit @ Thu Nov 24 18:22:00 IST 2022

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
 //for(let i = 0;i<5;i++){
 //    console.log(i)
 //}
-// synchronous 
+// synchronous programming
 let a = prompt("What is your Name?")
 let b = prompt("What is your Age?")
 let c = prompt("What is your favorite player?")
 console.log(a + " is " + b + " years old and has " + c + " favorite color.");
 
     
+setTime
