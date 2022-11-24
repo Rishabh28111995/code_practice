0945cbeae9a5ae0cf6a55fac9abe7c1cb3c0f51f
rishabhcenation <rishabhcenation28@gmail.com>
2022-11-24 13:33:47 +0530

    commit @ $(date)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log("hello worrrrllllld");
+//console.log("hello worrrrllllld");
 
-for(let i = 0;i<5;i++){
-    console.log(i)
-}
+//for(let i = 0;i<5;i++){
+//    console.log(i)
+//}
+let a = prompt("What is your name?")
+let b = prompt("What is your age?")
