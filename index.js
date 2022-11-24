4b651fa5e8b51d37fd98ec8a92d638b342714e81
rishabhcenation <rishabhcenation28@gmail.com>
2022-11-24 19:09:01 +0530

    commit @ Thu Nov 24 19:09:00 IST 2022

--- index.js
+++ index.js
@@ -26,5 +26,4 @@ function loadScript(src, callback) {
     document.body.appendChild("script");
 }
 
-loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe
-)
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js")
