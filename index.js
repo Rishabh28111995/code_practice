5a3bcc2a348fb603755c56130ca7efb7743aad16
rishabhcenation <rishabhcenation28@gmail.com>
2022-11-24 15:35:00 +0530

    commit @ Thu Nov 24 15:35:00 IST 2022

--- index.js
+++ index.js
@@ -5,19 +5,5 @@
 //for(let i = 0;i<5;i++){
 //    console.log(i)
 //}
-let n = 5;
-let string = "";
-// External loop
-for (let i = 1; i <= n; i++) {
-  // printing spaces
-  for (let j = 1; j <= n - i; j++) {
-    string += " ";
-  }
-  // printing star
-  for (let k = 0; k < 2 * i - 1; k++) {
-    string += "*";
-  }
-  string += "\n";
-}
-console.log(string); 
+
     
