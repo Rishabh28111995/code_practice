7d1cc3c602d01ae9e4bd9c87542310f588ecc233
rishabhcenation <rishabhcenation28@gmail.com>
2022-11-25 16:38:00 +0530

    commit @ Fri Nov 25 16:38:00 IST 2022

--- starpatterncode.js
+++ starpatterncode.js
@@ -1,17 +1,1 @@
-let n = 5;
-let string = "";
-// External loop
-for (let i = 1; i < n; i++) {
-  // printing spaces
-  for (let j = 1; j < n - i; j++) {
-    string += "-";
-  }
-  // printing star
-  for (let k = 0; k < 2 * i - 1; k++) {
-    string += "*";
-  }
-  string += "\n";
-  //console.log(string)
-}
-console.log(string); 
-    //reading about loops
+let height = 5;
