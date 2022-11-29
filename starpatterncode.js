e0404b2e86dc3284faab2ff5d866fc8103194a81
rishabhcenation <rishabhcenation28@gmail.com>
2022-11-29 10:28:00 +0530

    commit @ Tue Nov 29 10:28:00 IST 2022

--- starpatterncode.js
+++ starpatterncode.js
@@ -4,7 +4,7 @@ for(let i=1; i <= height; i++){
   for(let j = 1; j < height-i; j++){
        payload += " ";
   }  
-  for(let k = 0; k < 2*i-1; k++){
+  for(let k = ; k < 2*i-1; k++){
     payload += "*";
   }
   payload += "\n";
