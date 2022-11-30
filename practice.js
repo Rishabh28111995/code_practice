475feac9ff8504c668f6ad53611b2124f99b4e2b
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-01 00:11:00 +0530

    commit @ Thu Dec  1 00:11:00 IST 2022

--- practice.js
+++ practice.js
@@ -1,9 +1,9 @@
 let height = 5;
 let srting = "";
 for (let i = 0; i < 5; i++) {
-for (let j = 1; j< height-i; j++){
+for (let j = 1; j < height-i; j++){
     string += " ";
 }
-for ()
+for (let k = 0; k <)
     
 }
