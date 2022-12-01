5ab78c0044c221a6baec0d03fe5a2f4d2419276a
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-01 15:06:00 +0530

    commit @ Thu Dec  1 15:06:00 IST 2022

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let promise = new Promise(function(resolve, reject){
-    alert("HELLO")
+    
     resolve(56)
 })
 
 console.log("Hello Rishabh")
 setTimeout(function(){
     console.log("Hello to rishabh again in 5 seconds")
-}, 5000)
+}, 10000)
 console.log("my name is Rishabh")
 console.log(promise)
