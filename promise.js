05bdf26998faf01918f1c3d149f7872464b6e256
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-01 20:23:00 +0530

    commit @ Thu Dec  1 20:23:00 IST 2022

--- promise.js
+++ promise.js
@@ -5,4 +5,13 @@ let p = new Promise((resolve, reject)=>{
         reject(new Error("I am an Error"))
     }, 5000)
 })
+
+
+let p = new Promise((resolve, reject)=>{
+    console.log("Promise is pending")
+    setTimeout(()=>{
+        console.log("I am a promise and i am rejected")
+        reject(new Error("I am an Error"))
+    }, 5000)
+})
 console.log(p)
