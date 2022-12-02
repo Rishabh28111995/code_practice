b1ff470a522a1af0f887ae52b6f958520af065ca
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-02 15:39:00 +0530

    commit @ Fri Dec  2 15:39:00 IST 2022

--- callback.js
+++ callback.js
@@ -3,3 +3,5 @@ function loadScript(src) {
     script.src = src;
     document.body.appendChild(script);
 }
+
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js)
