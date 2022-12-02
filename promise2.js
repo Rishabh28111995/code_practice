3522bb8ec78bb185090d8f46832a9dfe6510aa38
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-02 12:46:00 +0530

    commit @ Fri Dec  2 12:46:00 IST 2022

--- promise2.js
+++ promise2.js
@@ -2,5 +2,10 @@ let p1 = new Promise((resolve, reject)=>{
     setTimeout(()=>{
         console.log("resolved after 3 seconds")
         resolve(56)
-    })
+    }, 3000)
+})
+
+p1.then((value)=>{
+
+    
 })
