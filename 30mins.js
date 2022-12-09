f69cd62bb72452198a06da60c2a17b46fc6cafcc
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-10 00:39:00 +0530

    commit @ Sat Dec 10 00:39:00 IST 2022

--- 30mins.js
+++ 30mins.js
@@ -5,7 +5,7 @@ console.log(myStr)
 var lastName = "Sharma";
 console.log(lastName.length - 1)
 console.log(lastName[5])
-console.log(myStr.length)
+console.log(myStr.length)*/
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
     var result = "";
     result += "The " + myAdjective + myNoun + myVerb + " to the store " + myAdverb.
