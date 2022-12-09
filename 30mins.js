a9df735fbf879104d00c601f8accb86cf416f16b
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-09 23:54:00 +0530

    commit @ Fri Dec  9 23:54:00 IST 2022

--- 30mins.js
+++ 30mins.js
@@ -3,5 +3,5 @@ var myStr = "My name is Rishabh."
 myStr += "Who are you?"
 console.log(myStr)
 var lastName = "Sharma";
-console.log(lastName.length)
+console.log(lastName.length - 1)
 console.log(lastName[5])
