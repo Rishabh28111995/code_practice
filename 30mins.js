d2524ef90c1f7da389bb1581261e15fe0c2d353f
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-10 00:41:00 +0530

    commit @ Sat Dec 10 00:41:00 IST 2022

--- 30mins.js
+++ 30mins.js
@@ -8,8 +8,8 @@ console.log(lastName[5])
 console.log(myStr.length)*/
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
     var result = "";
-    result += "The " +  myAdjective  +  myNoun  +  myVerb  +  " to the store " + myAdverb;
+    result += "The " +  myAdjective  +  myNoun  +  myVerb  + " to the store " + myAdverb;
     return result;
 
 }
-console.log(wordBlanks("dog", "big", "ran", "quickly"));
+console.log(wordBlanks(" dog" , " big ", " ran ", " quickly "));
