fc07e50255d3785cf0f2e92f68859bb56a57b2d6
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-15 18:34:00 +0530

    commit @ Thu Dec 15 18:34:00 IST 2022

--- functions.js
+++ functions.js
@@ -64,7 +64,11 @@ function testEqual(val) {
   if (val > 100) {
     return ("Over 100");
   } 
-  if val > 10) 
+  if (val > 10) {
+    return ("Over 10")
+
+  }
+  return ("10 or under"); 
  }
 
 
