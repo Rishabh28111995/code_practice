6f0fc1c5ceaec6431dbe9ed6293bf3d876d8ab10
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-16 02:11:00 +0530

    commit @ Fri Dec 16 02:11:00 IST 2022

--- functions.js
+++ functions.js
@@ -132,5 +132,7 @@ function golfScore(par, strokes){
     return names[1]
   }else if (strokes = par - 1){
     return names[2]
-  }else if (strokes = par)
+  }else if (strokes = par){
+    return names[3]
+  }else if (strokes)
 }
