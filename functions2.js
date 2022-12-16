5b154de4114dc744722a9c28f8e1777be8e31187
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-17 02:38:00 +0530

    commit @ Sat Dec 17 02:38:00 IST 2022

--- functions2.js
+++ functions2.js
@@ -38,7 +38,13 @@ function sequentialSizes(val) {
     switch(val) {
         case 1:
          case 2:
-            
+        case 3:
+            answer = "Low"
+            case 1:
+            case 2:
+            case 3:
+                   answer = "Low"
+
     }
 }
  
