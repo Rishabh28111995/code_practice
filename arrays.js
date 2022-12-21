f86a5be344896f33da2c7e65d4e0b214d8c9f7eb
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-21 22:58:00 +0530

    commit @ Wed Dec 21 22:58:00 IST 2022

--- arrays.js
+++ arrays.js
@@ -59,7 +59,7 @@ function multiplyAll(arr) {
 
       for (var i = 0; i < arr.length ;i++){
         for (var j = 0; j < arr[i].length; j++){
-            product *= arr[i][j]
+            product *= arr[i]
         }
       }
       return product
