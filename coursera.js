c961ce9817b2ff617ed724329bc535879ef488c9
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-23 02:12:00 +0530

    commit @ Fri Dec 23 02:12:00 IST 2022

--- coursera.js
+++ coursera.js
@@ -3,6 +3,8 @@ for (let i = 2023; i <= 2025; i++){
     console.log(i);
     for (let j = 6; j < 10; j++){
         console.log("-------", j)
-        
+        for( let k = 2; k < 4; k++){
+            console.log()
+        }
     }
 }
