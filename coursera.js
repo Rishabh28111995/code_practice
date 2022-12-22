64b6ae4e88d93d8d1c215dce47fa9adea36d9be9
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-23 02:11:00 +0530

    commit @ Fri Dec 23 02:11:00 IST 2022

--- coursera.js
+++ coursera.js
@@ -1,7 +1,8 @@
 // nested loop
-for (let i = 2023; i < 2025; i++){
+for (let i = 2023; i <= 2025; i++){
     console.log(i);
-    for (let j = 6; j < 9; j++){
-        console.log("")
+    for (let j = 6; j < 10; j++){
+        console.log("-------", j)
+        
     }
 }
