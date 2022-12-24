8ed3d010acc848a175ccf4033e3800c8e90f8053
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-24 09:35:00 +0530

    commit @ Sat Dec 24 09:35:00 IST 2022

--- coursera.js
+++ coursera.js
@@ -30,7 +30,11 @@ if(i == 0 && i == 1) {
 
   for (let i = 1; i < 11; i++){
     switch(i){
-       case 1: 
+       case 1:
+        console.log("Gold medal")
+        break;
+        case 2:
+            console.log("Silver medal") 
     }
   }
 
