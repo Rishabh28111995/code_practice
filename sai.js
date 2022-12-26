54770873a5c2f7e900854b0f700936b944472c18
rishabhcenation <rishabhcenation28@gmail.com>
2022-12-27 01:22:00 +0530

    commit @ Tue Dec 27 01:22:00 IST 2022

--- sai.js
+++ sai.js
@@ -25,7 +25,8 @@ console.log(createPerson("Rishabh", "27", "Male"));
 function factorial(n) {
     if (n === 0 || n === 1)
     return 1;}
-    else{for (let i = n; i >= 1; i--){
+    else{
+        for (let i = n; i >= 1; i--){
         n *= i;
     }
     return n;
