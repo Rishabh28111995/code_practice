f286e3d15f499e6e07e3e5db7fe27dea488f091e
rishabhcenation <rishabhcenation28@gmail.com>
2023-01-03 17:53:00 +0530

    commit @ Tue Jan  3 17:53:00 IST 2023

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let promise = new Promise(function(resolve, reject){
+// let promise = new Promise(function(resolve, reject){
     
-    resolve(56)
-})
+//     resolve(56)
+// })
 
-console.log("Hello Rishabh")
-setTimeout(function(){
-    console.log("Hello to rishabh again in 5 seconds")
-}, 10000)
-console.log("my name is Rishabh")
-console.log(promise)
+// console.log("Hello Rishabh")
+// setTimeout(function(){
+//     console.log("Hello to rishabh again in 5 seconds")
+// }, 10000)
+// console.log("my name is Rishabh")
+// console.log(promise)
