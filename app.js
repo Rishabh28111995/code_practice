35dbda5191ca40956e4b1713cd960e20701a8096
rishabhcenation <rishabhcenation28@gmail.com>
2023-02-12 12:22:00 +0530

    commit @ Sun Feb 12 12:22:00 IST 2023

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Modules
 const names = require('./2-names')
+const sayHi = require('./3-anything')
 
-sayHi('susan')
-sayHi('john')
-sayHi('peter')
+//sayHi('susan')
+//sayHi('john')
+//sayHi('peter')
