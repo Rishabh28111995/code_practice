fb6b5e2da7d70cfc297c8fa620aa832e6a7cf1e3
rishabhcenation <rishabhcenation28@gmail.com>
2023-02-12 12:18:00 +0530

    commit @ Sun Feb 12 12:18:00 IST 2023

--- 2-names.js
+++ 2-names.js
@@ -2,4 +2,4 @@ const secret = 'SUPER SECRET'
 const john = 'john'
 const peter = 'peter'
 
-console.log(module)
+module.exports = {john, peter}
--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 // Modules
-const secret = 'SUPER SECRET'
-const john = 'john'
-const peter = 'peter'
-
-const sayHi = (name) => {
-    console.log(`Hello there ${name}`)
-}
+const names = require('./')
 
 sayHi('susan')
 sayHi('john')
