6a95c8c269d23c5cf1033645ebac797854ae5e30
rishabhcenation <rishabhcenation28@gmail.com>
2023-02-15 10:19:00 +0530

    commit @ Wed Feb 15 10:19:00 IST 2023

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // Modules allows us to break up the code sep. files.
 const names = require('./2-names')
 const sayHi = require('./3-anything')
-
-const redValue = require('./4-addvalue')
+require('./4-addvalue')
 
 //sayHi('susan')
 //sayHi(names.john)
