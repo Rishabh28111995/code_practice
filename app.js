bf41b4f19dc6e38cdf21b3547f9e141aacc03a25
rishabhcenation <rishabhcenation28@gmail.com>
2023-02-15 10:18:00 +0530

    commit @ Wed Feb 15 10:18:00 IST 2023

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const names = require('./2-names')
 const sayHi = require('./3-anything')
 
 const redValue = require('./4-addvalue')
-console.log()
+
 //sayHi('susan')
 //sayHi(names.john)
 //sayHi(names.peter)
