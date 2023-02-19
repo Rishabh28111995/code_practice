ef6da276d249f3507f9aedd860d8275d2bfa746a
rishabhcenation <rishabhcenation28@gmail.com>
2023-02-19 09:20:00 +0530

    commit @ Sun Feb 19 09:20:00 IST 2023

--- app2.js
+++ app2.js
@@ -1,6 +1,8 @@
 //OS module.
 const os = require('os') // built in module
 
-
+// info about current user
+const user = os.userInfo()
+co
 
 console.log(`The system uptime is ${os.uptime()} seconds`)
