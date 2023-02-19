05981d9bce5f64b8448f5a3e61a2f9b9e8bc7f33
rishabhcenation <rishabhcenation28@gmail.com>
2023-02-19 09:25:00 +0530

    commit @ Sun Feb 19 09:25:00 IST 2023

--- app2.js
+++ app2.js
@@ -10,5 +10,6 @@ console.log(`The system uptime is ${os.uptime()} seconds`)
 const currentOS = {
     name:os.type(),
     release:os.release(),
-    
+    totalMem:os.totalmem(),
+    freeMem:os.freemem
 }
